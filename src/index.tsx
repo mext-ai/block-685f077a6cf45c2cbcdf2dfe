c3793abb9a516c3986db68a1a9380373e771d6e0
fabiensabatie <fabiensabatie@users.noreply.github.com>
2025-06-27 21:48:13 +0000

    ⚡ Optimize React component logic

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import { useGLTF, useTexture } from '@react-three/drei'
 import 'inter-ui'
 
 useTexture.preload('../public/textures/heightmap_1024.png')
-useGLTF.preload('../public/models/track-draco.glb')
-useGLTF.preload('../public/models/chassis-draco.glb')
-useGLTF.preload('../public/models/wheel-draco.glb')
+useGLTF.preload('https://content.mext.app/uploads/c9050d26-971c-47be-aeca-4d12261b64a8.glb')
+useGLTF.preload('https://content.mext.app/uploads/ffcbdbfc-7ea6-47c8-b952-479d31ff64db.glb')
+useGLTF.preload('https://content.mext.app/uploads/3827436c-5b86-4225-8f43-5ab4a5b8d78c.glb')
 
 // Use the mount function directly
 const container = document.getElementById('root')!;
